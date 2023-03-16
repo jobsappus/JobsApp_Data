a8286d8d930110fd9d37e169d9b4049612ee3813
jobsappus <Jobsappus@gmail.com>
2023-03-16 17:11:07 +0530

    edit company bug fixed

--- src/components/Company.js
+++ src/components/Company.js
@@ -95,7 +95,7 @@ export default function Company() {
 												</td>
 												<td className="relative whitespace-nowrap py-4 pl-5 pr-4 text-right text-sm font-medium sm:pr-0">
 													<Link
-														to={`/company/edit/${'as'}`}
+														to={`/company/edit/${company.companyId}`}
 														className="text-indigo-600 hover:text-indigo-900"
 													>
 														Edit
